ded7001d4d9f12e336efd95ea8c010add0eb8ac5
Graham Markall <gmarkall@nvidia.com>
2021-05-21 12:32:20 +0100

    Add author info in setup.py

--- setup.py
+++ setup.py
@@ -69,9 +69,9 @@ class CMakeBuild(build_ext):
 setup(
     name='optix',
     version='0.0.1',
-    author='Dean Moldovan',
-    author_email='dean0x7d@gmail.com',
-    description='A test project using pybind11 and CMake',
+    author='Keith Morley',
+    author_email='kmorley@nvidia.com',
+    description='Python bindings for NVIDIA OptiX',
     long_description='',
     ext_modules=[CMakeExtension('optix')],
     cmdclass=dict(build_ext=CMakeBuild),
