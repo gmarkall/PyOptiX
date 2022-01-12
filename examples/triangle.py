9fd65c322ac5616fb89d1119ad73f8c35e4e9248
Michael Wang <michaelwang0905@icloud.com>
2022-01-12 13:12:14 -0800

    optix trace is working...?

--- examples/triangle.py
+++ examples/triangle.py
@@ -678,11 +678,12 @@ def lower_optix_Trace(context, builder, sig, args):
     n_payload_registers = 3
     n_stub_output_operands = 32 - n_payload_registers
     outputs = ([p0, p1, p2] +
-               [builder.alloca(ir.IntType(32))
+               [builder.load(builder.alloca(ir.IntType(32)))
                 for _ in range(n_stub_output_operands)])
 
 
-    asm = ir.InlineAsm(ir.FunctionType(ir.VoidType(), []),
+    retty = ir.LiteralStructType([ir.IntType(32)] * 32)
+    asm = ir.InlineAsm(ir.FunctionType(retty, []),
     "call "
     "(%0,%1,%2,%3,%4,%5,%6,%7,%8,%9,%10,%11,%12,%13,%14,%15,%16,%17,%18,%19,%20,%21,%22,%23,%24,%25,%26,%27,%28,%"
     "29,%30,%31),"
@@ -690,11 +691,12 @@ def lower_optix_Trace(context, builder, sig, args):
     "(%32,%33,%34,%35,%36,%37,%38,%39,%40,%41,%42,%43,%44,%45,%46,%47,%48,%49,%50,%51,%52,%53,%54,%55,%56,%57,%58,%"
     "59,%60,%61,%62,%63,%64,%65,%66,%67,%68,%69,%70,%71,%72,%73,%74,%75,%76,%77,%78,%79,%80);",
     "=r," * 32 + "r,l,f,f,f,f,f,f,f,f,f,r,r,r,r,r,r," + "r," * 31 + "r",
+    side_effect=True
     )
 
     zero = context.get_constant(types.int32, 0)
     c_payload_registers = context.get_constant(types.int32, n_payload_registers)
-    args = outputs + [zero, handle, ox, oy, oz, dx, dy, dz, tmin, tmax, rayTime,
+    args = [zero, handle, ox, oy, oz, dx, dy, dz, tmin, tmax, rayTime,
                        visibilityMask, rayFlags, SBToffset, SBTstride,
                        missSBTIndex, c_payload_registers] + outputs
     return builder.call(asm, args)
